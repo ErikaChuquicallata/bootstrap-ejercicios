08025949911a3ccd9cc015a41fca7a832f0e6aa3
erika nataly <erika_nataly.123@hotmail.com>
2016-03-10 12:16:44 -0500

    pagina nataly

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scaler=1">
+	<meta http-equiv="X-UA-compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="public/css/bootstrap.css">
+	<link rel="stylesheet" type="test/css"href ="estilos.css">
+</head>
+<h1>         ♥ Erika Nataly ♥         </h1>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4"><img src="public/img/1.jpg" class="img-responsive" alt=""></div>
+			<div class="col-md-4"><img src="public/img/2.jpg" class="img-responsive" alt=""></div>
+			<div class="col-md-4"><img src="public/img/3.jpg" class="img-responsive" alt=""></div>
+		</div>
+		
+		<audio autobuffer autoloop loop controls autoplay="true">
+			<source src="public/audio/Lejos_de_ti_-Dailyn_curbelo.mp3">
+			
+		</audio>
+
+
+
+	</div>
+
+</body>
+</html>
